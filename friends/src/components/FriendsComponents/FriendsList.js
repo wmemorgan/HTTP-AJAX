91273808d553d446cf0939516f563ae378adbcf8
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-04-03 12:19:17 +0800

    Add rendering of friends list

--- friends/src/components/FriendsComponents/FriendsList.js
+++ friends/src/components/FriendsComponents/FriendsList.js
@@ -2,16 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import { FriendsListContainer } from './FriendsStyleComponents'
+import Friend from './Friend'
 
 const FriendsList = props => {
-  const { data } = props
+  const { friends } = props
   return (
     <FriendsListContainer>
-      data.map(friend => {
-        <Link key={friend.id} to={`/friends/${friend.id}`}>
-          <Friend friend={friend}/>
-        </Link>
-      })
+      {friends.map(friend => (
+          <Link key={friend.id} to={`/friends/${friend.id}`}>
+            <Friend friend={friend} />
+          </Link>
+        ))}
     </FriendsListContainer>
   )
 }
