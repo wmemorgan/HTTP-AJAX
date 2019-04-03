0c29d1df58f221f14a124d0e15b70ad9ba2ae75a
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-04-03 11:23:48 +0800

    Add state
    Add routes

--- friends/src/App.js
+++ friends/src/App.js
@@ -6,11 +6,38 @@ import AppContainer from './components/StyleComponents/AppStyles'
 import FriendsList from './components/FriendsComponents/FriendsList'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+    .get('http://localhost:5000/friends')
+    .then(response => {
+      this.setState({friends: response.data}, 
+        () => console.log(`CDM invoked state is: `, this.state))
+    })
+    .catch(error => console.log(`Data retrieval error: ${error}`))
+  }
 
   render() {
+    console.log(`render state is: `, this.state)
     return (
       <AppContainer>
-        <FriendsList />
+        {/* Add routes */}
+        <Route 
+          exact path='/'
+          render={props => <FriendsList {...props} {...this.state}/>} 
+        />
+        {this.state.friends.map(friend => (
+          <Route 
+            key={friend.id}
+            path={`/friends/${friend.id}`}
+          />
+          ))}
       </AppContainer>
     );
   }
