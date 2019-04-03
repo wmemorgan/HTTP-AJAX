d638712ca7b0f92fdc0ab71173f85655193d6872
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-04-03 14:12:42 +0800

    Add component preview

--- friends/src/components/FriendsComponents/FriendsList.js
+++ friends/src/components/FriendsComponents/FriendsList.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import { FriendsListContainer } from './FriendsStyleComponents'
-import Friend from './Friend'
+import { FriendsListContainer, Preview } from './FriendsStyleComponents'
+// import Friend from './Friend'
 
 const FriendsList = props => {
   const { friends } = props
   return (
     <FriendsListContainer>
       {friends.map(friend => (
-          <Link key={friend.id} to={`/friends/${friend.id}`}>
-            <Friend friend={friend} />
-          </Link>
-        ))}
+        <Link to={`/friends/${friend.id}`}>
+          <Preview key={friend.id}>
+            {friend.name}
+          </Preview>
+        </Link>
+    ))}
     </FriendsListContainer>
   )
 }
--- friends/src/components/FriendsComponents/FriendsStyleComponents.js
+++ friends/src/components/FriendsComponents/FriendsStyleComponents.js
@@ -3,16 +3,37 @@ import { color, colorScheme, fontSizing, flex } from '../StyleComponents/theme'
 
 export const FriendsListContainer = styled.div`
   width: 90%;
+  ${flex('column','center','center')}
   max-width: 600px;
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
   padding: 20px 10px;
   background: ${color.lightText};
-  & a {
+  font-size: ${fontSizing.sm};
+
+  a {
     text-decoration: none;
-    color: ${colorScheme.defaultFontColor}
+    color: ${colorScheme.defaultFontColor};
   }
+
+
+`
+
+export const Preview = styled.div`
+  width: 90%;
+  min-width: 300px;
+  ${flex('column', 'flex-start', 'center')}
+  margin: 10px 0;
+  padding: 10px;
+  border: 0.5px solid ${color.primaryBgShading};
+  box-shadow:  5px 5px 5px 0px rgba(89,89,89,1);
+  background: ${color.primaryBgShading};
+  cursor: pointer;
+
+  &:hover {
+    color: ${color.lightText};
+    background: ${colorScheme.defaultFontColor};
+  } 
+
+
 `
 
 export const FriendInfoContainer = styled.div`
