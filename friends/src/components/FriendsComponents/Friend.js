295b5a4e01314ebffa9006b653f3d0c3fe1ad1cc
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-04-05 08:05:49 +0800

    Add update button to Friend component

--- friends/src/components/FriendsComponents/Friend.js
+++ friends/src/components/FriendsComponents/Friend.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from "prop-types"
 
-import { FriendInfoContainer } from './FriendsStyleComponents'
+import { FriendInfoContainer, ButtonMenu } from './FriendsStyleComponents'
 import Button from '../StyleComponents/Button'
 
 const Friend = props => {
+  console.log(`Friend props: `, props)
   const { name, id, age, email } = props.friend
   return (
     <FriendInfoContainer>
@@ -12,7 +14,12 @@ const Friend = props => {
       <p><span className="stat-category">ID:</span> {id}</p>
       <p><span className="stat-category">Age:</span> {age}</p>
       <p><span className="stat-category">Email:</span> {email}</p>
-      <Button delete onClick={() => props.deleteFriend(id)}>Delete</Button>
+      <ButtonMenu>
+        <Link to={`/update/${id}`}>
+          <Button update>Update</Button>
+        </Link>
+        <Button delete onClick={() => props.deleteFriend(id)}>Delete</Button>
+      </ButtonMenu>
     </FriendInfoContainer>
   )
 }
--- friends/src/components/FriendsComponents/FriendsStyleComponents.js
+++ friends/src/components/FriendsComponents/FriendsStyleComponents.js
@@ -40,28 +40,28 @@ export const Preview = styled.div`
     background: ${colorScheme.defaultFontColor};
   } 
 
-
 `
 
 export const FriendInfoContainer = styled.div`
   ${flex('column','flex-start','center')}
   width: 80%;
   min-width: 300px;
+  max-width: 600px;
   margin: 20px 0;
-  padding: 10px;
+  padding: 20px;
   border-radius: 5px;
   box-shadow:  10px 10px 5px 0px rgba(0,0,0,0.75);
   background: ${color.lightText};
 
   h3 {
     margin: 5px 0;
-    font-size: ${fontSizing.s};
+    font-size: ${fontSizing.m};
     font-weight: bold;
     color: ${colorScheme.headingColor};
   }
 
   p {
-    font-size: ${fontSizing.xs};
+    font-size: ${fontSizing.s};
     margin: 5px 0;
   }
 
@@ -69,13 +69,38 @@ export const FriendInfoContainer = styled.div`
     font-weight: bold;
   }
 
-  button {
-    width: 100px;
-    align-self: center;
-    margin-top: 20px;
+  @media ${breakpoints[0]} {
+    h3 {
+      font-size: ${fontSizing.sm};
+    }
+
+    p {
+      font-size: ${fontSizing.xs};
+    }
   }
 
+`
 
+export const ButtonMenu = styled.nav`
+  align-self: center;
+  width: 80%;
+  margin-top: 20px;
+  ${flex('row','center','space-evenly')};
+
+  button {
+    width: 100px;
+    font-size: ${fontSizing.xs};
+  }
+
+  @media ${breakpoints[0]} {
+    ${flex('column', 'center', 'center')};
+
+    button {
+      width: 70px;
+      font-size: ${fontSizing.xxs};
+      margin-bottom: 20px;
+    }
+  }
 `
 
 
